e08c1277821ba74209314aed252be47d64332e30
sedstan <sedky.stanley.said@gmail.com>
2015-11-02 17:39:15 +0000

    Fixing user index and show pages

--- Just_Had/config/routes.rb
+++ Just_Had/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   
   root "users#index"
-  get 'users/index'
-  get 'users/show'
+
   resources :suggestions
   devise_for :users
+  resources :users, only: [:index, :show]
 
 end

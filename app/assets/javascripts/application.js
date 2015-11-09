9a06ae8decaf72d994a53ae416392f30e7235852
sedstan <sedky.stanley.said@gmail.com>
2015-11-09 13:46:35 +0000

    Removing masonry

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,13 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
-//= require masonry/jquery.masonry
-//= require masonry/jquery.event-drag
-//= require masonry/jquery.imagesloaded.min
-//= require masonry/jquery.infinitescroll.min
-//= require masonry/modernizr-transitions
-//= require masonry/box-maker
-//= require masonry/jquery.loremimages.min
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
